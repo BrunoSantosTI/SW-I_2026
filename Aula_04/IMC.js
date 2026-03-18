2c84ed6b31657b25e61ef250e62dd98cf3f72e30
BrunoSantosTI <brunnof.santosti@gmail.com>
2026-03-18 08:13:54 -0300

    Add files via upload

--- Aula_04/IMC.js
+++ Aula_04/IMC.js
@@ -1,24 +1,30 @@
 function calculoIMC() {
     var peso = parseFloat(document.getElementById("peso").value)
     var altura = parseFloat(document.getElementById("altura").value)
-    var temp=(altura*altura);
-    var IMC = peso/temp;
+    
+    if (isNaN(peso) || isNaN(altura) || altura === 0) {
+        alert("Digite valores válidos!");
+        return;
+    }
 
-    // if (IMC<18.5){
-    //     var grauOb = 0;
-    // } else if(IMC<24.9) {
-    //     var grauOb = 0;
-    // } else if(IMC<29.9) {
-    //     var grauOb = 1;
-    // } else if(IMC<39.9) {
-    //     var grauOb = 2;
-    // } else{
-    //     var grauOb = 3;
-    // }
+    var IMC = (peso/(altura*altura)).toFixed(2);
+    var grauOb;
 
-    document.getElementById("resultadoIMC").innerText = "IMC: " + peso;
+    if (IMC<18.5){
+        grauOb = 0;
+    } else if(IMC<24.9) {
+        grauOb = 0;
+    } else if(IMC<29.9) {
+        grauOb = 1;
+    } else if(IMC<39.9) {
+        grauOb = 2;
+    } else{
+        grauOb = 3;
+    }
 
-    document.getElementById("resultadoOb").innerText = "Grau de Obesidade: " + altura;
+    document.getElementById("resultadoIMC").innerText = "IMC: " + IMC;
+
+    document.getElementById("resultadoOb").innerText = "Grau de Obesidade: " + grauOb;
 
     
 }
