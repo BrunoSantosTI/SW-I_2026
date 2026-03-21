e86dbdb7cf10f696e009a74798cba9b4911aa8a5
BrunoSantosTI <brunnof.santosti@gmail.com>
2026-03-20 21:15:12 -0300

    Add files via upload

--- Aula_05/AtividadeForms/index.php
+++ Aula_05/AtividadeForms/index.php
@@ -9,28 +9,49 @@
 <body>
     <main>
         <div class="container">
-            <form action="cad-php" method="post">
-                <div class="areaNota">
-                    <label for="nome">Nome</label>
-                    <input class="inputNome" type="text" name="nome" id="idnome" placeholder="Seu primeiro nome...">
-        
-                    <label for="sobrenome">Sobrenome</label>
-                    <input class="inputNome" type="text" name="sobrenome" id="idsobrenome" placeholder="Seu sobrenome...">
+            <form method="post">
+                <h1 class="titulo">Cálculo de Média</h1>
+                <div class="areaNome">
+                    <div class="labelNome">
+                        <label for="nome">Nome</label>
+                        <input class="inputNome" type="text" name="nome" id="idnome" placeholder="Seu primeiro nome...">
+                    </div>
+
+                    <div class="labelNome">
+                        <label for="sobrenome">Sobrenome</label>
+                        <input class="inputNome" type="text" name="sobrenome" id="idsobrenome" placeholder="Seu sobrenome...">
+                    </div>  
                 </div>
 
                 <div class="areaNota">
-                    <label for="n1">Nota 1</label>
-                    <input class="inputNota" type="text" name="n1" id="idn1" placeholder="Sua primeira nota...">
-        
-                    <label for="n2">Nota 2</label>
-                    <input class="inputNota" type="text" name="n2" id="idn2" placeholder="Sua segunda nota...">
-        
-                    <label for="n3">Nota 3</label>
-                    <input class="inputNota" type="text" name="n3" id="idn3" placeholder="Sua terceira nota...">
+                    <div class="labelNota">
+                        <label for="n1">Nota 1</label>
+                        <input class="inputNota" type="text" name="n1" id="idn1" placeholder="Sua primeira nota...">
+                    </div>
+
+                    <div class="labelNota">
+                        <label for="n2">Nota 2</label>
+                        <input class="inputNota" type="text" name="n2" id="idn2" placeholder="Sua segunda nota...">
+                    </div>
+
+                    <div class="labelNota">
+                        <label for="n3">Nota 3</label>
+                        <input class="inputNota" type="text" name="n3" id="idn3" placeholder="Sua terceira nota...">
+                    </div>                    
                 </div>
     
                 <input class="btnEnviar" type="submit" value="Enviar">
             </form>
+            <?php
+                $_nome = $_POST["nome"];
+                $_sobrenome = $_POST["sobrenome"];
+                $_n1 = $_POST["n1"];
+                $_n2 = $_POST["n2"];
+                $_n3 = $_POST["n3"];
+                $_media= (($_n1+$_n2+$_n3)/3);
+                echo "<p>Seja muito bem-vindo(a), $_nome $_sobrenome</p> <br> <p>Sua média final ficou em: $_media </p>";
+            ?>
+
         </div>
     </main>
 </body>
