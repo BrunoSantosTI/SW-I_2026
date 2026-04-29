421ed982387e820be25c54884ca4339eb2b36f9b
BrunoSantosTI <brunnof.santosti@gmail.com>
2026-04-29 08:48:28 -0300

    Add files via upload

---
+++ Aula_07/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <p>Bem-vindo ao mundo do aprendizado com Duolingo!</p>
+    <p>Aprender um novo idioma nunca foi tão fácil, divertido e acessível. O Duolingo transforma o estudo em uma experiência interativa, com lições rápidas, desafios diários e recompensas que mantêm você motivado.</p>
+    <p>Aqui você pode explorar diferentes idiomas, praticar no seu ritmo e acompanhar seu progresso de forma simples. Seja para viagens, estudos ou crescimento pessoal, o Duolingo é a ferramenta ideal para começar hoje mesmo.</p>
+</body>
+</html>
